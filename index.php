20b934e7fd90f544a926791b449143137cb07fd1
Riona Dsouza <rionadsouza30@gmail.com>
2022-04-07 16:16:34 +0530

    commit

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 	<header>
 		<div class="container-fluid">
 			<nav class="navbar navbar-expand-lg fixed-top shadow rounded">
-				<a class="navbar-brand" href="index.html">
+				<a class="navbar-brand" href="index.php">
 					<img src="images/logo5.png" height="32px" class="d-inline-block align-top" alt="" loading="lazy">
 						
 				</a>
@@ -47,7 +47,7 @@
 						<a class="nav-link" href="#contact">Contact</a>
 						</li>
 						<li class="nav-item">
-						<a class="nav-link" href="login.html"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+						<a class="nav-link" href="login.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
 							<path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
 							<path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
 						  </svg></a>
